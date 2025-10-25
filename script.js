cdbb54f957c69a9831613b18427b3837a823e5bb
GabmartinsPro <pro.gabrielmartins.n@gmail.com>
2025-10-25 10:40:32 -0300

    Novas funcionalidades e assets

---
+++ script.js
@@ -0,0 +1,9 @@
+function abrirMenu() {
+        // Abre o menu, definindo a largura para 250px
+        document.getElementById("menuLateral").style.width = "250px";
+    }
+
+    function fecharMenu() {
+        // Fecha o menu, definindo a largura para 0
+        document.getElementById("menuLateral").style.width = "0";
+    }
